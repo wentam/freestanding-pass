8fdbe37b270fcf6a1da349d67ecfdd43b381380d
wentam <wentam42@gmail.com>
2022-12-15 19:18:54 -0500

    patch in PATH environment

--- flake.nix
+++ flake.nix
@@ -8,16 +8,20 @@
   flake-utils.lib.eachDefaultSystem (
     system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
     in rec {
       packages.default = pkgs.stdenv.mkDerivation {
         name = "freestanding-pass";
         src = ./.;
         makeFlags = [ "prefix=$(out)" ];
         postPatch = ''
-          substituteInPlace src/freestanding-gpg \
-            --replace "gpg" "${pkgs.gnupg}/bin/gpg"
-          substituteInPlace src/freestanding-pass \
-            --replace "pass \$@" "${pkgs.pass}/bin/pass \$@"
+          # We've placed a UUID comment for patching stuff into each source file
+          substituteInPlace src/* \
+            --replace "#ff3a228e-39fc-4c4e-9aa5-09e144da397a" "export PATH=\"''${out}/bin:${lib.makeBinPath ([
+            pkgs.coreutils
+            pkgs.pass
+            pkgs.gnupg
+          ])}\""
         '';
       };
       apps.default = { type = "app"; program = "${packages.default}/bin/freestanding-pass"; };
